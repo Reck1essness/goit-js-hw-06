fccb9f7afc963783cddc03fee524d41648c392fd
Reck1essness <crossdmitry@gmail.com>
2025-08-01 22:51:39 +0300

    3rd task

---
+++ js/task-3.js
@@ -0,0 +1,28 @@
+class StringBuilder {
+  #value;
+  constructor(initialValue) {
+    this.#value = initialValues;
+  }
+  getValue() {
+    return this.#value;
+  }
+  padEnd(str) {
+    this.value += str;
+  }
+  padBoth(str) {
+    this.value = str + this.#value + str;
+  }
+  padStart(str) {
+    this.value = str + this.value;
+  }
+}
+
+
+const builder = new StringBuilder(".");
+console.log(builder.getValue()); // "."
+builder.padStart("^");
+console.log(builder.getValue()); // "^."
+builder.padEnd("^");
+console.log(builder.getValue()); // "^.^"
+builder.padBoth("=");
+console.log(builder.getValue()); // "=^.^="
